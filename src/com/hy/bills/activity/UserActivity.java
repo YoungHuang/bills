2e72cdbbb2447076702c49f02cd5d5ededd221cc
yonghuang <h00138136@163.com>
2012-07-31 16:58:37 +0800

    Add user edit or create dialog

--- src/com/hy/bills/activity/UserActivity.java
+++ src/com/hy/bills/activity/UserActivity.java
@@ -2,14 +2,23 @@ package com.hy.bills.activity;
 
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,6 +30,7 @@ public class UserActivity extends BaseActivity {
 	private static final String TAG = "UserActivity";
 
 	private UserService userService;
+	private UserListAdapter userListAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,23 +46,94 @@ public class UserActivity extends BaseActivity {
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				toggleSlideMenu();
 				if (position == 0) {
-					// showUserAddOrEditDialog();
+					showUserAddOrEditDialog(null);
 				}
 			}
 		});
 
+		// 显示用户列表
 		ListView userListView = (ListView) findViewById(R.id.userList);
-		UserListAdapter userListAdapter = new UserListAdapter();
+		userListAdapter = new UserListAdapter();
 		userListView.setAdapter(userListAdapter);
 
+		// 设置ContextMenu
+		registerForContextMenu(userListView);
+
 		// 设置标题
-		String title = getString(R.string.UserActivityTitle, new Object[] {userListAdapter.getCount()});
+		String title = getString(R.string.user_activity_title, new Object[] { userListAdapter.getCount() });
 		setTitle(title);
 	}
 
 	private void initVariables() {
 		MainApplication application = (MainApplication) getApplicationContext();
 		userService = application.getUserService();
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo adapterContextMenuInfo = (AdapterContextMenuInfo) menuInfo;
+		User user = (User) userListAdapter.getItem(adapterContextMenuInfo.position);
+		menu.setHeaderIcon(R.drawable.user_small_icon);
+		menu.setHeaderTitle(user.getName());
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.edit_delete_menu, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo adapterContextMenuInfo = (AdapterContextMenuInfo) item.getMenuInfo();
+		User user = (User) userListAdapter.getItem(adapterContextMenuInfo.position);
+		switch (item.getItemId()) {
+		case R.id.edit:
+			editUser(user);
+			break;
+		case R.id.delete:
+			deleteUser(user);
+			break;
+		default:
+			return super.onContextItemSelected(item);
+		}
+
+		return true;
+	}
+
+	private void showUserAddOrEditDialog(User user) {
+		View view = LayoutInflater.from(this).inflate(R.layout.user_add_edit_dialog, null);
+		EditText editText = (EditText) view.findViewById(R.id.userName);
+		String title = "";
+		if (user != null) {
+			editText.setText(user.getName());
+			title = getString(R.string.user_dialog_title, new Object[] { getString(R.string.edit) });
+		} else {
+			title = getString(R.string.user_dialog_title, new Object[] { getString(R.string.create) });
+		}
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(title).setIcon(R.drawable.user_big_icon).setView(view);
+		builder.setNeutralButton(R.string.save, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		});
+		builder.setNegativeButton(R.string.cancel, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		builder.show();
+	}
+
+	private void editUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void deleteUser(User user) {
+		// TODO Auto-generated method stub
+
 	}
 
 	private class UserListAdapter extends BaseAdapter {
