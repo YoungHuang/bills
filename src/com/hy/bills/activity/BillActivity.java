d59833d9f80a6fcb2c327cf00b5a515ff8373fc0
yonghuang <h00138136@163.com>
2012-08-31 18:02:41 +0800

    update

--- src/com/hy/bills/activity/BillActivity.java
+++ src/com/hy/bills/activity/BillActivity.java
@@ -1,14 +1,21 @@
 package com.hy.bills.activity;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.hy.bills.MainApplication;
 import com.hy.bills.domain.AccountBook;
+import com.hy.bills.domain.Bill;
 import com.hy.bills.service.AccountBookService;
 import com.hy.bills.service.BillService;
 
@@ -17,6 +24,7 @@ public class BillActivity extends BaseActivity {
 	
 	private BillService billService;
 	private AccountBookService accountBookService;
+	private BillListAdapter billListAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,8 @@ public class BillActivity extends BaseActivity {
 		
 		AccountBook accountBook = accountBookService.getDefaultAccountBook();
 		ListView billListView = (ListView) findViewById(R.id.billListView);
+		billListAdapter = new BillListAdapter();
+		billListView.setAdapter(billListAdapter);
 		
 		// 创建滑动菜单
 		createSlideMenu(R.array.SlideMenuBillActivity, new OnItemClickListener() {
@@ -45,5 +55,57 @@ public class BillActivity extends BaseActivity {
 		MainApplication application = (MainApplication) getApplicationContext();
 		billService = application.getBillService();
 		accountBookService = application.getAccountBookService();
+	}
+	
+	private class BillListAdapter extends BaseAdapter {
+		private List<Bill> billList;
+
+		public BillListAdapter() {
+			billList = billService.findAll();
+		}
+
+		@Override
+		public int getCount() {
+			return billList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return billList.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Holder holder;
+			if (convertView == null) {
+				convertView = LayoutInflater.from(BillActivity.this).inflate(R.layout.bill_list_item, null);
+				holder = new Holder();
+				convertView.setTag(holder);
+			} else {
+				holder = (Holder) convertView.getTag();
+			}
+
+			Bill bill = billList.get(position);
+
+			return convertView;
+		}
+
+		private class Holder {
+			TextView billDate;
+			TextView totalAmount;
+			TextView billName;
+			TextView amount;
+			TextView billUsersAndType;
+		}
+
+		public void dataChanged() {
+			billList = billService.findAll();
+			this.notifyDataSetChanged();
+		}
 	}
 }
